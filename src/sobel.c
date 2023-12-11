a614e9124c4e45aee7233c93c3c3c6f2862d4a3c
“beuve” <nicolas@beuve.net>
2023-12-11 18:17:13 +0100

    Skiable

--- src/sobel.c
+++ src/sobel.c
@@ -28,7 +28,10 @@ void sobel(int width, int height, unsigned char *input, unsigned char *output) {
                input[(j + 1) * width + i - 1] + 2 * input[(j + 1) * width + i] +
                input[(j + 1) * width + i + 1];
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> Skiable
       output[(j-1)*width + i] = fminf(sqrtf(gx * gx + gy * gy), 255);
     }
   }
